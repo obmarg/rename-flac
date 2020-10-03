c1da06de80ce3e920f1a266aefac5c35733d86d4
Graeme Coupar <grambo@grambo.me.uk>
2020-10-03 20:48:08 +0100

    Make it so

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::{ffi::OsStr, path::PathBuf};
+use flac::metadata::get_vorbis_comment;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opts {
+    /// Files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    for filename in opts.files {
+        if filename.extension() != Some(OsStr::new("flac")) {
+            continue;
+        }
+
+        let metadata = get_vorbis_comment(filename.to_str().unwrap()).unwrap();
+
+        let artist = metadata.comments.get("ARTIST");
+        let title = metadata.comments.get("TITLE");
+
+        match artist.zip(title) {
+            Some((artist, title)) => {
+                let mut rename = filename.clone();
+                rename.set_file_name(format!("{} - {}.flac", artist, title));
+                println!("Renaming {} to {}", filename.to_string_lossy(), rename.to_string_lossy());
+                std::fs::rename(filename, rename).unwrap();
+            },
+            None => {
+                println!("{} did not have an artist or title", filename.to_string_lossy());
+            }
+        }
+    }
+}
